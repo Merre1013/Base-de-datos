8b0236c6521c2128b3d7b048e7826ceddccd6418
unknown <merre-1013@hotmail.com>
2024-05-30 23:01:55 -0500

    Actualizaciones

--- Practica clase4.sql
+++ Practica clase4.sql
@@ -4,14 +4,14 @@ use Escuela;
 
 
 create table: Curso(
-id int AUTO_INCREMENT identity PRIMARY KEY,
+id int AUTO_INCREMENT  PRIMARY KEY,
 nombre VARCHAR (100),
 descripcion TEXT (100),  
 
 )
 
 Create table:  Estudiante(
-id int AUAUTO_INCREMENTTO identity PRIMARY KEY,
+id int AUAUTO_INCREMENTTO  PRIMARY KEY,
 nombre VARCHAR (100),
 correo VARCHAR (100),
 FOREIGN KEY (id_curso) references curso(id),
@@ -28,7 +28,7 @@ FOREIGN KEY (id_estudiante) references estudiante(id),
 
 
 Create table:  Profesor(
-id int AUTO_INCREMENT  identity PRIMARY KEY;
+id int AUTO_INCREMENT  PRIMARY KEY;
 nombre VARCHAR (100),
 departamento VARCHAR (100),  
 
@@ -36,7 +36,7 @@ departamento VARCHAR (100),
 
 Create table:  Curso_Asignado(
 id_curso int ,
-id _asignado int,
+id_profesor int,
 PRIMARY KEY (id_curso, id_profesor),
 FOREIGN KEY (id_curso) references curso(id),
 FOREIGN KEY (id_profesor) references profesor(id),
